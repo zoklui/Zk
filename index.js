faae6ed1090255f2371371c7ae2a0de287d97736
zoklui <154670714+zoklui@users.noreply.github.com>
2024-01-02 21:24:59 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express()
 const PORT = 4000
 
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
+  console.log(`<h1>API listening on PORT ${PORT} `)
 })
 
 app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
+  res.send('<h1>Hey this is my API running ok 🥳</h1>')
 })
 
 app.get('/about', (req, res) => {
