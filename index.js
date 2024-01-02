dac187465d22d99659be5189cb8d40e871d756a3
zoklui <154670714+zoklui@users.noreply.github.com>
2024-01-02 09:49:01 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+{
+  "name": "Zokou_parring_Code",
+  "version": "1.0.0",
+  "description": "Connect Whatsapp With Baileys Code Pairing",
+  "main": "index.js",
+  "engines" :  {
+
+    "node" : "18.x"
+  } ,
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node . --use-pairing-code"
+  },
+  "keywords": [
+    "baileys",
+    "code-pairing"
+  ],
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/rizzlogy/zyypairing.git"
+  },
+  "author": "RizzyFuzz",
+  "license": "ISC",
+  "dependencies": {
+    "@adiwajshing/keyed-db": "^0.2.4",
+    "@colors/colors": "1.5.0",
+    "@whiskeysockets/baileys": "^6.4.0",
+    "cfonts": "~2.10.0",
+    "chalk": "~4.1.0",
+    "express": "^4.18.2",
+    "fs-extra": "^11.1.1",
+    "path": "^0.12.7",
+    "pino": "^8.1.0"
+  },
+  "bugs": {
+    "url": "https://github.com/rizzlogy/zyypairing/issues"
+  },
+  "homepage": "https://github.com/rizzlogy/zyypairing#readme"
+}
